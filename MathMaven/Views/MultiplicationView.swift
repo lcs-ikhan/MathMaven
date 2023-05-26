59f7f012b9e21e264bceb6b070a708a050828d2c
Isaad Khan <IKhan@lcs.on.ca>
2023-05-26 08:02:28 -0400

    Fixed multiplication

--- MathMaven/Views/MultiplicationView.swift
+++ MathMaven/Views/MultiplicationView.swift
@@ -8,14 +8,131 @@
 import SwiftUI
 
 struct MultiplicationView: View {
-    var body: some View {
-        VStack {
-            Text("Multiplication")
-            Text(Operation.multiplication.rawValue)
+
+        // MARK: Stored properties
+        
+        // The numbers to be subtracted
+        @State var firstValue = Int.random(in: 1...20)
+        @State var secondValue = Int.random(in: 1...20)  // Will be updated once view loads
+        
+        // Holds whatever input was provided by the user
+        @State var input = ""
+        
+        // Has the user's answer been checked?
+        @State var answerChecked = false
+        
+        // Was the user's given answer correct?
+        @State var answerCorrect = false
+        
+        // MARK: Computed properties
+
+        // The correct response
+        var correctResponse: Int {
+            return firstValue * secondValue
         }
-        .font(Font.custom("SF Pro", size: 64))
+        
+        // The user interface
+        var body: some View {
+            VStack(spacing: 0) {
+                
+                // 1. Present question
+                Group {
+                    HStack {
+                        Text(Operation.multiplication.rawValue)
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .trailing) {
+                            Text("\(firstValue)")
+                            Text("\(secondValue)")
+                        }
+                    }
+                    
+                    Divider()
+                }
+                .padding(.horizontal)
+
+                // 2. Accept answer
+                HStack {
+
+                    ZStack {
+                        
+                        // Only show this when the answer was found to be correct
+                        if answerCorrect == true {
+                            Image(systemName: "checkmark.circle")
+                                .foregroundColor(.green)
+                        }
+                        
+                        // Show this when the answer was checked and found to be false
+                        if answerChecked == true && answerCorrect == false {
+                            Image(systemName: "x.square")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    TextField("",
+                              text: $input)
+                        .multilineTextAlignment(.trailing)
+                }
+                .padding(.horizontal)
+
+                // 3. Check answer
+                //    Only show button when answer has not already been checked
+                if answerChecked == false {
+                    
+                    CheckAnswerButtonView(input: input,
+                                          correctResponse: correctResponse,
+                                          answerChecked: $answerChecked,
+                                          answerCorrect: $answerCorrect)
+                    
+                } else {
+                    
+                    // 4. Generate new question
+                    // Only show this once an answer has been provided
+                    Button(action: {
+                        generateNewQuestion()
+                    }, label: {
+                        Text("New question")
+                            .font(.largeTitle)
+                    })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    
+                }
+                
+                // Push interface up to top of screen
+                Spacer()
+
+            }
+            .font(Font.custom("SF Pro", size: 64))
+            // Runs as soon as the view loads
+            .task {
+                // Changes second value from 0 to something less than first value
+                secondValue = Int.random(in: 1...firstValue)
+            }
+        }
+        
+        // Generate a new question
+        func generateNewQuestion() {
+            
+            // Generate a new question
+            firstValue = Int.random(in: 1...20)
+            secondValue = Int.random(in: 1...firstValue)
+
+            // Reset properties that track what's happening with the current question
+            answerChecked = false
+            answerCorrect = false
+            
+            // Reset the input field
+            input = ""
+
+        }
     }
-}
+
+    
+
 
 struct MultiplicationView_Previews: PreviewProvider {
     static var previews: some View {
